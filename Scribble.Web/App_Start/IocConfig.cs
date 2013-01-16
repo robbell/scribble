6288e8dc1e4cb3407dec80046c3ac6295d6bafa7
Rob Bell <me@rob-bell.net>
2013-01-16 23:27:23 +0000

    Added controller IoC registration

---
+++ Scribble.Web/App_Start/IocConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using Autofac;
+using Autofac.Integration.Mvc;
+
+namespace Scribble.Web
+{
+    public class IocConfig
+    {
+        public static void Register()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
