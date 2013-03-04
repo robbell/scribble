ab53d9df6e49aa7550b6be3177106b99ca8e8fa8
Rob Bell <me@rob-bell.net>
2013-03-04 22:27:20 +0000

    Added RavenDB IoC configuration

--- Scribble.Web/App_Start/IocConfig.cs
+++ Scribble.Web/App_Start/IocConfig.cs
@@ -1,6 +1,9 @@
 ﻿using System.Web.Mvc;
 using Autofac;
 using Autofac.Integration.Mvc;
+using Raven.Client;
+using Raven.Client.Embedded;
+using Scribble.Web.Models;
 
 namespace Scribble.Web
 {
@@ -9,10 +12,33 @@ namespace Scribble.Web
         public static void Register()
         {
             var builder = new ContainerBuilder();
+
             builder.RegisterControllers(typeof(MvcApplication).Assembly);
+            builder.RegisterType<ConcreteRepo>().As<IPostRepository>();
+
+            RegisterRavenDb(builder);
 
             var container = builder.Build();
+
             DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+
+        private static void RegisterRavenDb(ContainerBuilder builder)
+        {
+            builder.Register(c =>
+            {
+                var documentStore = new EmbeddableDocumentStore();
+                documentStore.DataDirectory = "Data";
+                documentStore.Initialize();
+
+                return documentStore;
+            })
+            .As<IDocumentStore>()
+                .SingleInstance();
+
+            builder.Register(c => c.Resolve<IDocumentStore>().OpenSession())
+                .As<IDocumentSession>()
+                .InstancePerHttpRequest();
         }
     }
 }
