1796e1166c6413138462733241194ad89e5a836f
Rob Bell <me@rob-bell.net>
2013-05-19 19:11:59 +0100

    Added missing PostTest

---
+++ Scribble.Tests/Models/PostTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using Scribble.Web.Models;
+
+namespace Scribble.Tests.Models
+{
+    [TestFixture]
+    public class PostTest
+    {
+        [Test]
+        public void UrlTitleIsLastSectionOfUrl()
+        {
+            var post = new Post { Url = "2012/03/expected-title" };
+            Assert.That(post.UrlTitle, Is.EqualTo("expected-title"));
+        }
+    }
+}
