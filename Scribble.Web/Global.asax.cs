9b42f80124a8577291b558722e3f1db3323eeada
Rob Bell <me@rob-bell.net>
2015-06-16 20:30:11 +0100

    Run IoC before creating attributes

--- Scribble.Web/Global.asax.cs
+++ Scribble.Web/Global.asax.cs
@@ -9,12 +9,12 @@ namespace Scribble.Web
     {
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
+            IocConfig.Register();
 
+            AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            IocConfig.Register();
         }
     }
 }
