b02c90371513e34a4e8f6565f301538d959ff225
Rob Bell <me@rob-bell.net>
2015-11-08 19:46:01 +0000

    Add basic JS comment submission using Angular

---
+++ Scribble.Web/Scripts/controllers.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    'use strict';
+
+    var scribbleControllers = angular.module('scribbleControllers', []);
+
+    scribbleControllers.controller('CommentCtrl', [
+        '$scope', '$http',
+        function ($scope, $http) {
+            $scope.comment = {};
+
+            $scope.addComment = function (isValid) {
+                if (isValid) {
+                    $http.post('/comments/add', $scope.comment).success(function () {
+                        alert("added!");
+                    });
+                }
+            };
+        }
+    ]);
+})();
